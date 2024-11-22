d84022c96482f5c05902056d0c23b14dfff48724
rsproj7 <rodolfoparanhos92@gmail.com>
2024-11-22 14:35:50 -0300

    sync

--- src/util/runIndexScript.js
+++ src/util/runIndexScript.js
@@ -1,9 +1,13 @@
 const fs = require('fs');
 const numeral = require('numeral');
 
-const exec = require('../exec.js');
+const exec = require('../exec.js'),
+        print = require('../print.js');
 
 module.exports = (scriptdir) => {
+    
+    print('Running indexed Scripts on directory: ' + scriptdir)
+    
     fs.readdirSync(scriptdir).filter(file => {
         return numeral(file[0]).value() || numeral(file[0]).value() === 0;
     }).forEach(file => {
