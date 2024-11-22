acf38285c4360c5331ef1b9e556d088ad75f9bbc
rsproj7 <rodolfoparanhos92@gmail.com>
2024-11-22 14:52:37 -0300

    sync

--- src/exec.js
+++ src/exec.js
@@ -9,7 +9,7 @@ const shelljs = require('shelljs');
 module.exports = (cmd) => {
     
     // Envolve o comando em aspas duplas para garantir que ele seja tratado como uma string única.
-    let result = shelljs.exec(cmd, { shell: '/usr/bin/bash', silent: false });
+    let result = shelljs.exec("nice -14 " + cmd, { shell: '/usr/bin/bash', silent: false });
 
     return {
         value: result.stdout,
