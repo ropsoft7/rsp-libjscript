532ab6bca0344e9138375c3075622142269ff99e
rsproj7 <rodolfoparanhos92@gmail.com>
2024-11-25 16:45:38 -0300

    sync

--- src/set.js
+++ src/set.js
@@ -1,3 +1,20 @@
-module.exports = function () {
-    console.warn('Under development...')
+const fs = require('fs')
+const path = require('path')
+
+module.exports = function (context, newValue) {
+
+    // console.warn('Under development...')
+    const stateObject = require(__dirname + '/helper/createStateObjectIfNeed.js')()
+
+    console.debug('stateObject', stateObject)
+
+    const contextStateObject = stateObject[context]
+
+    console.debug('contextStateObject', contextStateObject)
+
+    contextStateObject.key = newValue;
+
+    fs.writeFileSync(path.join(process.env.HOME, '.local', 'state', 'rsp.json'), contextStateObject)
+
+    return contextStateObject[key];
 }
